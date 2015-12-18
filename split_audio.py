5bd219dc2bd1db7f53cda17a6d87623f888e84af
wwj <wuwenjie718@gmail.com>
2015-12-18 17:40:25 +0800

    递归处理目录

--- split_audio.py
+++ split_audio.py
@@ -13,21 +13,23 @@
 
 '''
 import sys
+import os
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 from audio_config import min_silence_len,silence_thresh,keep_silence,filename_prefix
 
 
-def export_audio(audio_chunk,filename_prefix="unit1_word",index=1,audio_format="mp3"):
-    audio_chunk.export("{filename_prefix}{index}.mp3".format(filename_prefix=filename_prefix,index=str(index).zfill(3)), format=audio_format)
+def export_audio(audio_chunk,filename,index=1,audio_format="mp3"):
+    audio_chunk.export(filename,format=audio_format)
 
-
-if __name__ == '__main__':
+def split_audio(path_to_file):
+    #找到当前目录下的所有MP3，在同级目录下生成
     print u"你选择的参数为\nmin_silence_len : {min_silence_len}\nsilence_thresh : {silence_thresh}\nkeep_silence : {keep_silence}\nfilename_prefix : {filename_prefix}".format(min_silence_len=min_silence_len,silence_thresh=silence_thresh,keep_silence=keep_silence,filename_prefix=filename_prefix)
     print "*"*20
-    audio_file = sys.argv[1]
-    print u"正在为你切割{}文件".format(audio_file)
-    myAudio = AudioSegment.from_mp3(audio_file)
+    #audio_file = sys.argv[1]
+    audio_file = path_to_file
+    print "正在为你切割{}文件".format((path_to_file))
+    myAudio = AudioSegment.from_mp3(path_to_file)
     chunks = split_on_silence(myAudio,
         #保证可切部分的稳定性,是个稳定的背景音
         min_silence_len = min_silence_len,
@@ -39,7 +41,25 @@ if __name__ == '__main__':
     total_words = len(chunks)
     print "一共将{0}切割为{1}个单词,文件正在生成中，请稍后......".format(audio_file,str(total_words))
 
+    dirname = os.path.dirname(path_to_file)
+
     for i, chunk in enumerate(chunks):
-        export_audio(chunk,filename_prefix=filename_prefix,index=i+1)
+        filename = os.path.join(dirname,"{filename_prefix}_w{index}.mp3".format(filename_prefix="wwj",index=str(i).zfill(3)))
+        export_audio(chunk,filename,index=i+1)
 
     print "OK~ 一共生成{total_words}个mp3文件，从{filename_prefix}1.mp3到{filename_prefix}{total_words}.mp3\n".format(total_words=str(total_words),filename_prefix=filename_prefix)
+
+if __name__ == '__main__':
+    # python split_audio.py [path]
+    begin_path = sys.argv[1]
+    for dirname, dirnames, filenames in os.walk(begin_path):
+    # print path to all subdirectories first.
+        #for subdirname in dirnames:
+        #    print os.path.join(dirname, subdirname)
+        #print path to all filenames.
+        for filename in filenames:
+            #print os.path.join(dirname, filename)
+            if "mp3" in filename:
+                #print os.path.join(dirname, filename)
+                split_audio(os.path.join(dirname, filename))
+
